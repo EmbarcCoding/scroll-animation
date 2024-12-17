ee5ddf770698a296cdc4b269429668a48ebb43cb
Mitchell <mwurm819@gmail.com>
2024-12-16 23:47:06 -0600

    commit

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const observer = new IntersectionObserver((entries) => {
         console.log(entry)
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
         }
     });
 });
 
 
 const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((e1) => observer.observe(el));
